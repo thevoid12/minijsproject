be249db8017b2be78c719ee9a0141a4df85675e7
the_void1 <iamvinod2001@gmail.com>
2023-07-26 19:57:04 +0530

    dad joke using api both async/awake and .then

---
+++ proj10/script.js
@@ -0,0 +1,34 @@
+const jokeEl=document.getElementById('joke');
+const jokeBtn= document.getElementById('btn');
+jokeBtn.addEventListener('click',generateJoke)
+
+generateJoke()
+
+//using ASYNC/AWAIT
+async function generateJoke() //since fetch is async we need to write awaite(await till the func get over). if we use await then we should mention async infront of function
+{
+    const config={
+        headers:{
+            'Accept':'application/json'
+        }
+    }
+   const resp=await fetch('https://icanhazdadjoke.com',config) //will return a promise so await
+    
+   const data=await res.json() //will return a promise
+   jokeEl.innerHTML=data.joke;
+}
+
+//USING .THEN
+// function generateJoke()
+// {
+//     const config={
+//         headers:{
+//             'Accept':'application/json'
+//         }
+//     }
+//     fetch('https://icanhazdadjoke.com',config)
+//     .then(res=>res.json())
+//     .then(data=>{
+//         jokeEl.innerHTML=data.joke;
+//     })
+// }
